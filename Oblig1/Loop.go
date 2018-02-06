d41b24f12ac3c882e49592f5959d5bc5dc12a423
Prestholm <32932715+Prestholm@users.noreply.github.com>
2018-02-06 13:39:50 +0100

    Add files via upload

---
+++ Oblig1/Loop.go
@@ -0,0 +1,36 @@
+package main
+
+
+import "fmt"
+import "os"
+import "os/signal"
+import "syscall"
+
+//Chan står for "channel":
+func main() {
+	sign := make(chan os.Signal, 1)
+	quitCh := make(chan bool, 1)
+
+	//Valg av signal (SIGINT):
+	signal.Notify(sign, syscall.SIGINT)
+
+	//Kjører programmet:
+	go func() {
+		run := <-sign
+		fmt.Println(run)
+		quitCh <- true
+	}()
+
+	//Evig løkke:
+	for i := 1;  ; i++ {
+		fmt.Println("Venter på signal...")
+		select {
+		default: i++
+
+			//Avslutningsmelding ved mottatt signal (Ctrl + C):
+		case <- quitCh:
+			fmt.Println("-------------------Signal mottatt!-------------------")
+			os.Exit(1)
+		}
+	}
+}
