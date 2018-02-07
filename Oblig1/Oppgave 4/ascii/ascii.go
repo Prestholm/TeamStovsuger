11fe8b41b649e41e38ebc101ca75c78adc2ea101
Henningermaskin <35727241+Henningermaskin@users.noreply.github.com>
2018-02-07 13:13:30 +0100

    Update ascii.go

--- Oblig1/Oppgave 4/ascii/ascii.go
+++ Oblig1/Oppgave 4/ascii/ascii.go
@@ -19,10 +19,10 @@ func IterateOverASCIIStringLiteral(sl string) {
 
 // Kode for Oppgave 2b
 func ExtendedASCIIText()string {
-	t1 := []byte("\x80\xf7\xbe\x24")
+	t1 := []byte("\x80\xf7\xbe\x64\x6f\x6c\x6c\x61\x72")
 	for i := 0; i < len(t1); i++ {
 		fmt.Printf("%c", t1[i])
 	}
-	t2 := "\x80\xf7\xbe\x24"
+	t2 := "\x80\xf7\xbe\x64\x6f\x6c\x6c\x61\x72"
 	return t2
 }
