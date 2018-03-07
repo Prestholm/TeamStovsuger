833970662217f3e0ab4512ab6b14a3ecfbd33944
Prestholm <victor.prestholm@gmail.com>
2018-03-07 12:39:55 +0100

    Mappestruktur

--- Oblig2/src/oppgave2/filecount.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Programmet skal lese en tekst-fil, skrive ut totalt antall linjer og antall for fem runes som forekommer hyppigst i filen")
-}
