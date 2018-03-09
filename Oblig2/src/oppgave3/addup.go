19efa349bd738d2eba046ca33f48f8e5264fe0de
Arian <35727069+Nuskard@users.noreply.github.com>
2018-03-09 14:04:47 +0100

    Update addup.go

--- Oblig2/src/oppgave3/addup.go
+++ Oblig2/src/oppgave3/addup.go
@@ -6,27 +6,29 @@ import (
 
 )
 
-
 func main(){
-	numba := flag.Int("a", 100, "int")
-	numbb := flag.Int("b", 20, "int")
-	// satt standard nummere for a og b til 100 og 20
-	//
-	flag.Parse()
-	// har man endret verdi i terminalen så settes det inn i Parse()
-	c := make(chan int, 2)
-	c <- *numba
-	c <- *numbb
-	// legger til tallene i channel c
-	b(c)
-	// kommuniserer med den andre funskjonen til å legge sammen de to tallene og henter det ut igjen via channels
-	h := <-c
-	//definerer h-verdien til noe som kan printes ut og legges frem.
- fmt.Println(h)
 
+	var numba int
+	var numbb int
+
+	fmt.Println("Skriv inn nummer: ")
+	fmt.Scan(&numba)
+	fmt.Println("Skriv inn nummer: ")
+	fmt.Scan(&numbb)
+
+	flag.Parse()
+
+	c := make(chan int, 2)
+	c <- numba
+	c <- numbb
+
+	b(c)
+	
 }
+
 func b(c chan int){
 	b,g := <- c, <- c
 	c <- b+g
+	fmt.Println("Resultat: ", b+g)
 
 }
