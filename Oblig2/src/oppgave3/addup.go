36b38f942b5883b6ea84a1afe3c8be20cca971f5
Henningermaskin <35727241+Henningermaskin@users.noreply.github.com>
2018-03-09 13:11:06 +0100

    Update addup.go

--- Oblig2/src/oppgave3/addup.go
+++ Oblig2/src/oppgave3/addup.go
@@ -13,11 +13,15 @@ func main(){
 	// satt standard nummere for a og b til 100 og 20
 	//
 	flag.Parse()
+	// har man endret verdi i terminalen så settes det inn i Parse()
 	c := make(chan int, 2)
 	c <- *numba
 	c <- *numbb
+	// legger til tallene i channel c
 	b(c)
+	// kommuniserer med den andre funskjonen til å legge sammen de to tallene og henter det ut igjen via channels
 	h := <-c
+	//definerer h-verdien til noe som kan printes ut og legges frem.
  fmt.Println(h)
 
 }
