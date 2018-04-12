ebb5d45069af9d100340d6b9eabf6ce4027b61d7
Prestholm <victor.prestholm@gmail.com>
2018-04-12 18:19:01 +0200

    test

---
+++ Oblig3/oppg1/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+
+
+type people struct {
+	Message string `json:"message"`
+	Number int `json:"number"`
+}
+
+func main() {
+
+
+
+		url := "http://api.open-notify.org/astros.json"
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	people1 := people{}
+	jsonErr := json.Unmarshal(body, &people1)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+
+	fmt.Println(people1.Message)
+}
+
