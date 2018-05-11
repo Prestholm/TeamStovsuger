f0530038c737a8f545a857d013e112f3ec7decd6
Henningermaskin <35727241+Henningermaskin@users.noreply.github.com>
2018-05-12 00:04:11 +0200

    Update server.go

--- Oblig4/server.go
+++ Oblig4/server.go
@@ -12,48 +12,43 @@ import (
 	"io/ioutil"
 )
 
-const URL = "https://api.coinmarketcap.com/v1/ticker/" //Url til apien
+const URL = "https://api.coinmarketcap.com/v1/ticker/"
+const URL2 = "https://www.cryptocompare.com/api/data/coinlist/"
 
-/*Client varaible, setter 10 sec timeout på requests til endpointet. Foretrukket over å bruke standard http.Client,
-som har en timeout på 0 sec. For de som er interessert så kan de lese mer her:
-https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
-*/
 var client =&http.Client{Timeout: 10*time.Second}
 
 func main() {
 
-	http.HandleFunc("/", startSide) // Startsiden
-	http.HandleFunc("/info", basicHandler) // infosiden
-	http.HandleFunc("/list", liste) // liste for alle cryptoene
+	http.HandleFunc("/", startSide)
+	http.HandleFunc("/info", basicHandler)
+	http.HandleFunc("/list", liste)
 	if err := http.ListenAndServe(":8080", nil); err !=nil{
 		panic(err)
 	}
 	log.Println("Listening...")
 }
 func liste(w http.ResponseWriter, r *http.Request){
-	coins = new(Coins)
+	coins := new(Coins)
 	getData(URL, coins)
-	tpl.ParseFiles("list.html"	)
-	tpl.ExecuteTemplate(w, "list.html", coins) // kjører siden med coins som data, slik at den kan hente alt data URL har å gi på Coins{}
+	tpl.ParseFiles("index1.html"	)
+	tpl.ExecuteTemplate(w, "index1.html", coins)
 
 }
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-//Sjekker om metoden ikke er post.
+
 	if r.Method != "POST" {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
-		//Setter opp for FORMs (videre implementert i Form.html
 	n := r.FormValue("Name")
-	//struct for forms
 	d := struct {
 		Name string
 	}{
 		Name: strings.ToLower(n),
 	}
-	tpl.ExecuteTemplate(w, "print.html", d)// lager overskriften med bildene takket være dataen som henter navn
-	ID1 := r.PostFormValue("Name") // gjør navn om til string
-	res2, _ := http.Get(URL+ID1) // legger til URL sammen med den coinen man vil ha data for
+	tpl.ExecuteTemplate(w, "print.html", d)
+	ID1 := r.PostFormValue("Name")
+	res2, _ := http.Get(URL+ID1)
 	body2, _ := ioutil.ReadAll(res2.Body)
 	coin2 := Coins{}
 	err2 := json.Unmarshal(body2, &coin2)
@@ -61,36 +56,36 @@ func basicHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("error: ", err2)
 
 	}
-		//Gå gjennom det som ble fyllt ut i formsa.
 	err := r.ParseForm()
 	if err != nil{
 		panic(err)
 	}
-	//execute indexen vår med begrenset data som bare inneholder det URLen skal ha
+	tpl.ParseFiles("32x32/*")
 	tpl.ExecuteTemplate(w, "info.html", coin2)
 }
 
 var tpl *template.Template
-func getData(url string, target interface{})error{
-		//henter json-data fra url. Decoder denne dataen.
-		res,err := client.Get(url)
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
 
-		return json.NewDecoder(res.Body).Decode(target)
+func getData(url string, target interface{})error{
+	//henter json-data fra url. Decoder denne dataen.
+	res,err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	return json.NewDecoder(res.Body).Decode(target)
+
 }
 
 func init() {
 	tpl = template.Must(template.ParseGlob("Templates/*"))
 }
-//startsiden vår
 func startSide(w http.ResponseWriter, r *http.Request){
 	tpl.ParseFiles("form.html")
 	tpl.ExecuteTemplate(w, "form.html", nil)
 }
-//Slice struct som inneholder alt av info om hver crypto.
+
 type Coins []struct {
 
 	ID               string `json:"id"`
